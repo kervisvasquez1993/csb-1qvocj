0c28bd1cc4aef136e9017ca48c4cf6bf09c6f651
Code Sandbox <hello@codesandbox.io>
2022-10-14 04:05:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2dd1d47e5848501bfa7f7640cb5b939f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3317ba31af1b472dee0a65eb6f28f8b1",
+    "url": "https://kervisvasquez1993.github.io/csb-1qvocj/index.html"
+  },
+  {
+    "revision": "fb8ee96785564a8bb7af",
+    "url": "https://kervisvasquez1993.github.io/csb-1qvocj/static/css/main.bec094ea.chunk.css"
+  },
+  {
+    "revision": "e83fac4cef3b60cc7fdb",
+    "url": "https://kervisvasquez1993.github.io/csb-1qvocj/static/js/2.3a71b42d.chunk.js"
+  },
+  {
+    "revision": "fb8ee96785564a8bb7af",
+    "url": "https://kervisvasquez1993.github.io/csb-1qvocj/static/js/main.64222aec.chunk.js"
+  },
+  {
+    "revision": "7cdde51beb619c96fa40",
+    "url": "https://kervisvasquez1993.github.io/csb-1qvocj/static/js/runtime~main.8a7cd92e.js"
+  }
+]);
